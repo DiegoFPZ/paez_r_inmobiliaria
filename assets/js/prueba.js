aa953d41b4f676cb502047e57ad3942729bbb3f1
DiegoFPZ <isodyan@hotmail.com>
2026-03-05 10:38:13 -0500

    JS de prueba

--- assets/js/prueba.js
+++ assets/js/prueba.js
@@ -1,12 +1,3 @@
-// Tu primer código JavaScript útil
-console.log("¡Hola desde mi inmobiliaria!");
-
-const propiedadesPrueba = [
-    { nombre: "Casa en Bello", precio: 215000000 },
-    { nombre: "Apto Laureles", precio: 380000000 },
-    { nombre: "Local Centro", precio: 450000000 }
-];
-
-// Filtro básico (cuando aprendas)
-const propiedadesBaratas = propiedadesPrueba.filter(p => p.precio < 300000000);
-console.log("Propiedades económicas:", propiedadesBaratas);
+var persona;
+var persona = "Juan";
+persona
